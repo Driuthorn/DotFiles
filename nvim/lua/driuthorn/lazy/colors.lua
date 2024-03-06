7e69104d720bb1591d7dd3f9539e5fcbfbe2d64a
Driuthorn <douglas_doamaral@hotmail.com>
2024-03-05 22:58:41 -0300

    feat: Update colorschema. Removing background

--- nvim/lua/driuthorn/lazy/colors.lua
+++ nvim/lua/driuthorn/lazy/colors.lua
@@ -1,13 +1,41 @@
+function ColorMyPencils(color)
+    color = color or "rose-pine"
+    vim.cmd.colorscheme(color)
+
+    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+end
+
 return {
     { "folke/tokyonight.nvim",
-        lazy = false,
-        priority = 1000,
-        opts = {},
+        config = functiion()
+            local tn = require("tokyonight")
+            
+            tn.setup({
+                style = "storm",
+                transparent = true,
+                terminal_colors = true,
+                styles = {
+                    comments = { italic = false },
+                    keywords = { italic = false },
+                    sidebars = "dark",
+                    floats = "dark"
+                },
+            })
+        end
     },
     { "rose-pine/neovim", 
         name = "rose-pine",
         config = function() 
+            local rp == require('rose-pine')
+
+            rp.setup({
+                disable_background = true
+            })
+
             vim.cmd("colorscheme rose-pine")
+
+            ColorMyPencils()
         end
     },
 }
--- nvim/lua/driuthorn/lazy/nvimgelion.lua
+++ nvim/lua/driuthorn/lazy/nvimgelion.lua
@@ -1,5 +1,14 @@
 return {
     'nyngwang/nvimgelion',
     config = function() 
+        vim.api.nvim_create_autcmd({ 'COlorScheme', 'FileType'},
+        {
+            callback = function()
+                vim.cmd([[
+                    hi Normal guibg=NONE ctermbg=NONE
+                    hi EndOfBuffer guibg=NONE ctermbg=NONE
+                ]])
+            end
+        })
     end
 }
