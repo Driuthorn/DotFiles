d73441dd95a134bf64cc0a3b1911b2754b94a70b
Driuthorn <douglas_doamaral@hotmail.com>
2024-11-10 22:22:41 -0300

    feat: Update mason required library 'tsserver' to 'ts_ls'

--- nvim/.config/nvim/lua/driuthorn/lazy/lsp.lua
+++ nvim/.config/nvim/lua/driuthorn/lazy/lsp.lua
@@ -27,7 +27,7 @@ return {
         require("fidget").setup({})
         require("mason").setup()
         require("mason-lspconfig").setup({
-            ensure_installed = { "lua_ls", "rust_analyzer", "tsserver" },
+            ensure_installed = { "lua_ls", "rust_analyzer", "ts_ls" },
             handlers = {
                 function(server_name)
                     require("lspconfig")[server_name].setup { capabilities = capabilities }
