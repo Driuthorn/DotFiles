0d6fc94d5560b7d29a852cf74b73b049b35305c4
Driuthorn <douglas_doamaral@hotmail.com>
2026-05-23 10:11:42 -0300

    Feat: Add lsp autocompletion

--- nvim/.config/nvim/lua/driuthorn/plugins/telescope.lua
+++ nvim/.config/nvim/lua/driuthorn/plugins/telescope.lua
@@ -1,8 +1,4 @@
-local telescope = require('telescope')
-local actions = require('telescope.actions')
 local builtin = require('telescope.builtin')
-
-
 
 -- Keymaps
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
@@ -12,11 +8,11 @@ vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
 vim.keymap.set('n', '<leader>ps', function()
     builtin.grep_string({ search = vim.fn.input("Grep > ") });
 end)
-vim.keymap.set("n", "<leader>pws", function ()
+vim.keymap.set("n", "<leader>pws", function()
     local word = vim.fn.expand("<cword>")
     builtin.grep_string({ search = word })
 end)
-vim.keymap.set("n", "<leader>pWs", function ()
+vim.keymap.set("n", "<leader>pWs", function()
     local word = vim.fn.expand("<cWORD>")
     builtin.grep_string({ search = word })
 end)
